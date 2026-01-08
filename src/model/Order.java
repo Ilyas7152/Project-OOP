18840653c2d6912ccf271fa5d7115a06b5357293
llyas <khametov.777@mail.ru>
2026-01-09 00:58:21 +0500

    OOP project 3,4 week

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,77 +1,51 @@
 package model;
 
 public class Order {
-    private int Id;
+    private int id;
     private Product product;
     private Customer customer;
     private int quantity;
 
-
-    public Order(int Id, Product product, Customer customer, int quantity) {
-
-        this.Id = Id;
-        this.product = product;
-        this.customer = customer;
-        this.quantity = quantity;
-
+    public Order(int id, Product product, Customer customer, int quantity) {
+        setId(id);
+        setProduct(product);
+        setCustomer(customer);
+        setQuantity(quantity);
     }
 
-    public int getId() {
-        return Id;
-    }
+    public int getId() { return id; }
+    public int getQuantity() { return quantity; }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setOrderId(int Id) {
-        this.Id = Id;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
+    public void setId(int id) {
+        if (id > 0) this.id = id;
     }
 
     public void setQuantity(int quantity) {
-        this.quantity = quantity;
+        if (quantity > 0) this.quantity = quantity;
     }
 
+    public void setProduct(Product product) { this.product = product; }
+    public void setCustomer(Customer customer) { this.customer = customer; }
+
     public double getTotalPrice() {
-        double total= product.getPrice() * quantity;
-        if (quantity >=7){
-            total *=0.93;
-        } else if (quantity>=3) {
-            total *=0.97;
-            
-        }
+        double total = product.getPrice() * quantity;
+        if (quantity >= 7) total *= 0.93;
+        else if (quantity >= 3) total *= 0.97;
         return total;
     }
 
     public boolean completeOrder() {
         double total = getTotalPrice();
-        if (product.sellProduct(quantity) && customer.pay(total))
-        {System.out.println("Order was completed");
+        if (product.sellProduct(quantity) && customer.pay(total)) {
+            System.out.println("Order was completed");
             return true;
         }
-        System.out.println("Order was failed");
+        System.out.println("Order failed");
         return false;
     }
+
     @Override
-    public String toString(){
-        return "Order(Id: " +Id+", product: "+product+", Customer :"+ customer+",quantity :"+quantity+")";
+    public String toString() {
+        return "Order(id: " + id + ", product: " + product.getName() + ", customer: " + customer.getName() + ", qty: " + quantity + ")";
     }
-    }
-
-
+}
